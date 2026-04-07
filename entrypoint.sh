d9181e6982d1e4c17228901a85f748ba5954ed7e
hpriyap <148286469+hpriyap@users.noreply.github.com>
2026-04-07 00:09:05 -0400

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-source /opt/ros/humble/setup.bash
-if [ -f /workspace/ros2_ws/install/setup.bash ]; then
-    source /workspace/ros2_ws/install/setup.bash
-fi
-export PYTHONPATH=/workspace:$PYTHONPATH
-export DISPLAY=${DISPLAY:-:0}
-exec "$@"
